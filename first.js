0a64d2563280e39ba0092e5cc0b74da6dcf943c7
RANNAN <rannanrinu2220@gmail.com>
2026-07-09 11:07:48 +0530

    js learning started

---
+++ first.js
@@ -0,0 +1,30 @@
+console.log("welcome to javascript");
+console.log(typeof 2125);
+console.log(typeof "ufhehf");
+console.log(typeof true);
+var a
+console.log(a);
+var b=null
+console.log(b);
+
+// let bg=9007199254740991
+// let result=bg+1
+// console.log(result);
+
+// let bgg=9007199254740991
+// let result=bgg+2
+// console.log(result);
+
+let user={}
+user["name"]="rannn"
+user.age=21
+console.log(user["age"]);
+
+let age1=Symbol("age")
+let age2=Symbol("age")
+
+user[age1]=20
+user[age2]=21
+
+console.log(user[age1]);
+console.log(user[age2]);
---
+++ variables.js
@@ -0,0 +1,7 @@
+var firstData
+console.log(firstData);
+console.log(typeof firstData);
+var firstData="dfd"//reinnitialize
+console.log(firstData);
+console.log(typeof firstData);
+firstData
